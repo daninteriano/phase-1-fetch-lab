88b6a0a19ca5f898c6655c8e6d1115adc3b46b18
Daniel Interiano <dannyboyint@gmail.com>
2021-10-18 18:26:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((response) => response.json())
+  .then(function(jsonBooks){
+    return renderBooks(jsonBooks)
+  })
   
 }
 
@@ -15,3 +20,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
